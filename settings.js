b60113596eb7fbba3fa2a4c48c86b64c60ec68c4
FIREBALL252423 <117293644+FIREBALL252423@users.noreply.github.com>
2024-07-22 16:02:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 export default 
 {
     "minecraft_version": "1.20.4", // supports up to 1.20.4
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916,
+    "host": " AI12G7oG.aternos.me:25302", // or "localhost", "your.ip.address.here"
+    "port": 25302,
     "auth": "offline", // or "microsoft"
     
     "profiles": [
